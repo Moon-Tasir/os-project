4efae31e775848a5a355877717a764eec0add632
DESKTOP-DPB5JUG <moontasir65@gmail.com>
2021-08-31 17:30:23 +0600

    first commit

---
+++ logIN.js
@@ -0,0 +1,8 @@
+function logIn(){
+    const passwd = document.getElementById('pass').value;
+   const usrname= document.getElementById('usrnm').value;
+    
+    if(usrname==123 && passwd==123){
+        window.location.href='main.html';
+    }
+}
